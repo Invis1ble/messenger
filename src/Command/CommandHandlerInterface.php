938603158a299df5a2734d3be40add4558410695
Invis1ble <Invis1ble@users.noreply.github.com>
2023-09-04 01:54:04 +0300

    Add Message Handlers.

---
+++ src/Command/CommandHandlerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Invis1ble\Messenger\Command;
+
+use Invis1ble\Messenger\MessageHandlerInterface;
+
+interface CommandHandlerInterface extends MessageHandlerInterface
+{
+}
---
+++ src/Query/QueryHandlerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Invis1ble\Messenger\Query;
+
+use Invis1ble\Messenger\MessageHandlerInterface;
+
+interface QueryHandlerInterface extends MessageHandlerInterface
+{
+}
